337271960202e63c1530dc8cca351238719fe9e5
Administrator <xuetuxiaoliu@163.com>
2023-02-04 23:34:23 +0800

    liujizhi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("shit");
-            Console.WriteLine("666");
+            
             Console.ReadLine();
         }
     }
