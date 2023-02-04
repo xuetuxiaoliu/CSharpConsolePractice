482d297d0b432d23947cdcf84c4dc3afcb8c8cdb
Administrator <xuetuxiaoliu@163.com>
2023-02-05 00:52:13 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("shit");
-            
+            String name=Console.ReadLine();//readline可以1.用于停留,2.用于获取输入
+            int score = int.Parse(Console.ReadLine());//int.Parse将字符串解释成int
+            String info=string.Format("姓名:{0},分数:{1}",name,score);//占位符格式的字符串拼接
+            Console.WriteLine(info);
+            Console.WriteLine("姓名:{0},分数:{1}", name, score);//cw可以直接这么写
             Console.ReadLine();
         }
     }
